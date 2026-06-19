01bd2bffbed635f547bac21b88292c26939d7ddf
pipe732 <feliperodriguez080408@gmail.com>
2026-06-19 11:39:12 -0500

    feat: add HTML templates for inventory detail view, movement logs, and supplier management

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Producto, Categoria, Proveedor, Inventario, Movimientos, Detalle_Movimientos
+
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Proveedor)
+admin.site.register(Inventario)
+admin.site.register(Movimientos)
+admin.site.register(Detalle_Movimientos)
 
 # Register your models here.
