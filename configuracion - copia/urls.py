268ff4ef42eaed556f6500a282a342f7c72873af
pipe732 <feliperodriguez080408@gmail.com>
2026-08-12 11:09:57 -0500

    feat: add initial database migrations for all modules and update population script to automatically run migrations.

--- configuracion - copia/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.configuracion_view, name='configuracion'),
-    path('probar-conexion/', views.probar_conexion_neon, name='probar_conexion_neon'),
-]
